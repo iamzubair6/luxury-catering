8695f8e5f5d50e1c7ee8aea685463bc58b3ccce6
iamzubair6 <zubair.rahman89@gmail.com>
2024-07-05 21:20:42 +0600

    Header and footer complete

--- src/Components/Shared/Footer.tsx
+++ src/Components/Shared/Footer.tsx
@@ -1,5 +1,53 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="bg-[rgb(235,230,225)] py-10">
+        <div className="container mx-auto flex justify-between">
+          <div>
+            <h2 className="text-xl font-bold mb-2 font-fjalla">HOURS</h2>
+            <p>Open everyday</p>
+            <p>10am — 10pm</p>
+          </div>
+          <div>
+            <h2 className="text-xl font-bold mb-2 font-fjalla">SOCIALS</h2>
+            <ul>
+              <li>
+                <a href="#" className="hover:underline">
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Instagram
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-xl font-bold mb-2 font-fjalla">
+              LUXURY CATERING
+            </h2>
+            <p>017-55-66-44-55</p>
+            <p>
+              <a
+                href="mailto:orders@alphacateringservices.com"
+                className="hover:underline"
+              >
+                orders@alphacateringservices.com
+              </a>
+            </p>
+            <p>786 Ibrahimpur</p>
+            <p>Cantonment, Dhaka</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/Components/Shared/Header.tsx
+++ src/Components/Shared/Header.tsx
@@ -1,8 +1,11 @@
+import logo from "@assets/brand-logo/logo.svg";
+import { FaFacebook, FaInstagram } from "react-icons/fa6";
 import { Nav, NavLink } from "./Nav";
 
 const Header = () => {
   return (
-    <h1 className="sticky">
+    <div className="static flex items-center py-[20px] px-[60px] justify-between">
+      <img src={logo} alt="" className="h-[44px] w-[200px]" />
       <Nav>
         <NavLink to="/menu">Menu</NavLink>
         <NavLink to="/service">Service</NavLink>
@@ -10,7 +13,20 @@ const Header = () => {
         <NavLink to="/about">About</NavLink>
         <NavLink to="/contact">Contact</NavLink>
       </Nav>
-    </h1>
+      <div className="flex items-center gap-2">
+        <FaInstagram
+          size={20}
+          className="cursor-pointer transition-transform transform hover:scale-125 hover:text-pink-600"
+        />
+        <FaFacebook
+          size={20}
+          className="cursor-pointer transition-transform transform hover:scale-125 hover:text-blue-600"
+        />
+        <button className="bg-black text-white p-[24px] rounded-md ml-6 hover:bg-red-400">
+          +880 1XNN-NNNNNN
+        </button>
+      </div>
+    </div>
   );
 };
 
